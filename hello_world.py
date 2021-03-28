9df2c9cfabe9d9425d62b959fc7748932820198f
electro <electrocoder@gmail.com>
2021-03-28 13:15:04 +0300

    first commit

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+# https://docs.iota.org/docs/client-libraries/0.1/how-to-guides/python/send-your-first-bundle
+
+from iota import Iota
+from iota import ProposedTransaction
+from iota import Address
+from iota import Tag
+from iota import TryteString
+
+
+api = Iota('https://nodes.devnet.iota.org:443', testnet = True)
+
+address = 'ZLGVEQ9JUZZWCZXLWVNTHBDX9G9KZTJP9VEERIIFHY9SIQKYBVAHIMLHXPQVE9IXFDDXNHQINXJDRPFDXNYVAPLZAW'
+
+message = TryteString.from_unicode('Hello world sahin')
+
+
+tx = ProposedTransaction(
+address = Address(address),
+message = message,
+value = 0
+)
+result = api.send_transfer(transfers = [tx])
+
+print(result['bundle'].tail_transaction.hash)
+
---
+++ oku.py
@@ -0,0 +1,28 @@
+# https://docs.iota.org/docs/client-libraries/0.1/how-to-guides/python/read-transactions
+
+from iota import Iota
+
+api = Iota('https://nodes.devnet.iota.org:443', testnet = True)
+
+tail_transaction_hash = 'ADZRYHNKFQONJTNSRJIWPQTMRPICHONRGLQVOALTYEOWRUVYOBFQOMVCVFRUVMYLSSWYLNOBGXPLID999'
+
+bundle = api.get_bundles(tail_transaction_hash)
+
+message = bundle['bundles'][0].tail_transaction.signature_message_fragment
+print(message.decode())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
