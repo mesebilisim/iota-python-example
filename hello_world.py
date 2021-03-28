57ab2d147c6551ba5ba655aff827b4539e371d74
electro <electrocoder@gmail.com>
2021-03-28 13:19:10 +0300

    up

--- hello_world.py
+++ hello_world.py
@@ -6,20 +6,17 @@ from iota import Address
 from iota import Tag
 from iota import TryteString
 
-
-api = Iota('https://nodes.devnet.iota.org:443', testnet = True)
+api = Iota('https://nodes.devnet.iota.org:443', testnet=True)
 
 address = 'ZLGVEQ9JUZZWCZXLWVNTHBDX9G9KZTJP9VEERIIFHY9SIQKYBVAHIMLHXPQVE9IXFDDXNHQINXJDRPFDXNYVAPLZAW'
 
 message = TryteString.from_unicode('Hello world sahin')
 
-
 tx = ProposedTransaction(
-address = Address(address),
-message = message,
-value = 0
+    address=Address(address),
+    message=message,
+    value=0
 )
-result = api.send_transfer(transfers = [tx])
+result = api.send_transfer(transfers=[tx])
 
 print(result['bundle'].tail_transaction.hash)
-
